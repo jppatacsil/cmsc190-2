2f6b5c4c5d926f9c60cd8e0ea9058cd153356cb6
jppatacsil <jppatacsil.214@gmail.com>
2016-03-12 19:46:10 -0800

    First folder was empty

---
+++ INSTANT_SUITE/application/views/index.php
@@ -0,0 +1,82 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>INSTANT SUITE</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="homepage-css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="homepage-css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+   <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css"> 
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+
+    <!-- Header -->
+    <a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                 
+                        <h1> Instant Suite</h1>
+                        <h3>Online Examination System and Atttendance Checker</h3>
+                        <hr class="intro-divider">
+                        <ul class="list-inline intro-social-buttons">
+
+                            <li>
+                                <a href="login.php" class="btn btn-default btn-lg"><i class="fa fa-sign-in fa-fw"></i> <span class="network-name">Login</span></a>
+                            </li>
+                            <li>
+                                <a href="signup.php" class="btn btn-default btn-lg"><i class="fa fa-user fa-fw"></i> <span class="network-name">Sign Up</span></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn btn-default btn-lg"><i class="fa fa-google fa-fw"></i> <span class="network-name">Login via UP Mail</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.intro-header -->
+
+    <!-- jQuery -->
+    <script src="homepage-js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="homepage-js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
